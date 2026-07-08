bf6bc467c2bfbca245d7f7ff1bf119be5282b2cf
Chathmi Balasuriya <chathmibalasuriya@gmail.com>
2026-07-08 11:57:28 +0530

    feat: add survey API endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const healthRoutes = require("./routes/healthRoutes");
+const surveyRoutes = require("./routes/surveyRoutes");
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 // Health Check Route
 app.use("/health", healthRoutes);
+app.use("/api/surveys", surveyRoutes);
 
 module.exports = app;
