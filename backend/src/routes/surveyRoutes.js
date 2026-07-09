4f0e7052f085f82c44130c4a0f8d3b9c0e4af983
Chathmi Balasuriya <chathmibalasuriya@gmail.com>
2026-07-09 17:57:26 +0530

    feat: implement complete survey CRUD API

--- backend/src/routes/surveyRoutes.js
+++ backend/src/routes/surveyRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const surveyController = require("../controllers/surveyController");
 
 router.get("/", surveyController.getSurvey);
+router.get("/:id", surveyController.getSurveyById);
+router.put("/:id", surveyController.updateSurvey);
+router.delete("/:id", surveyController.deleteSurvey);
 router.post("/", surveyController.submitSurvey);
 
 module.exports = router;
